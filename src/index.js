ae8e679c0a9cf0d5dd282fe975c72b95a54baf5a
Caique Caleiro <caiquecaleiro@gmail.com>
2016-11-13 18:06:26 -0200

    Add initial project structure

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <h1>Weather app!</h1>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
+
